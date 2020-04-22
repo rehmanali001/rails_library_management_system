1dcfd4594d713462cab701a14fca0da66f49635a
Rehman Ali <rehmanali001@gmail.com>
2020-04-22 15:17:55 -0500

    fixed books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,23 @@
 class BooksController < ApplicationController
+    before_action :redirect_if_not_logged_in
+    
+    def new 
+        @book = Book.new
+    end 
+
+    def create
+        @book = current_user.books.build(book_params)
+        if @book.save
+            redirect_to books_path 
+        else   
+            render :new
+        end 
+    end 
+  
+
+    private 
+
+    def book_params
+        params.require(:book).permit(:title, :description)
+    end     
 end
