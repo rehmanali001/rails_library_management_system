32fef82651906310e8ade92d021252ea6af23d0f
Rehman Ali <rehmanali001@gmail.com>
2020-04-28 13:53:51 -0500

    added :id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -32,6 +32,6 @@ class CommentsController < ApplicationController
     private 
 
     def comment_params
-        params.require(:comment).permit(:content)
+        params.require(:comment).permit(:content, :book_id)
     end 
 end
