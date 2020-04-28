004cac2ebbd976837ad871de94290d6fe3f77841
Rehman Ali <rehmanali001@gmail.com>
2020-04-28 14:00:40 -0500

    adding nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,13 @@ Rails.application.routes.draw do
   
   resources :genres
   resources :comments
-  resources :users
-  resources :books
+  resources :users do 
+    resources :books, only: [:new, :create, :index]
+  end 
+  resources :books do
+    resources :comments, only: [:new, :create, :index]
+  end 
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
