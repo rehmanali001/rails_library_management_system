cd1d9063ed3597fbc365f5eba7f5a909d691cb84
Rehman Ali <rehmanali001@gmail.com>
2020-04-28 13:40:27 -0500

    adding methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,37 @@
 class CommentsController < ApplicationController
 
     def index 
-
+        @comments = Comment.all
     end 
 
     def new 
         @comment = Comment.new
+    end 
+
+    def create 
+        @comment = current_user.comments.build(comment_params)
+        if @comment.save 
+            redirect_to comments_path
+        else 
+            render :new 
+        end
+    end 
+
+    def show 
+        @comment = Comment.find_by(id: params[:id])
+    end 
+
+    def edit 
+        @comment = Comment.find_by(id: params[:id])
+    end 
+
+    def update
+
+    end 
+
+    private 
+
+    def comment_params
+        params.require(:comment).permit(:content)
     end 
 end
