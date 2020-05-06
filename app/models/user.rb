74ea581e42205bf9b264f90f71983c9a75a14d2d
Rehman Ali <rehmanali001@gmail.com>
2020-05-06 18:11:01 -0500

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
     has_many :commented_books, through: :comments, source: :book
     has_many :genres, through: :books
     has_secure_password        
+
+    validates :username, :email, presence: true
+
 end
