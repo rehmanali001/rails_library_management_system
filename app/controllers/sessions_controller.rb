356da926a9bc86e971e03ff08c5b91dc0f1abfba
Rehman Ali <rehmanali001@gmail.com>
2020-05-06 13:12:46 -0500

    added auth authentication

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,8 +11,28 @@ class SessionsController < ApplicationController
         end
     end 
 
+    def google 
+        @user = User.find_or_create_by(email: auth["info"]["email"]) do |user|
+            user.username = auth["info"]["first_name"]
+            user.password = SecureRandom.hex(10 )
+        end 
+        if @user.save 
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else  
+            redirect_to '/'
+        end
+    end  
+
     def destroy 
         session.clear 
         redirect_to root_path
     end 
+    
+    private 
+
+    def auth 
+        request.env['omniauth.auth']
+    end 
+
 end
