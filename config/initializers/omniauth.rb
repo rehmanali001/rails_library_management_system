c8082fc324012c2a89536c6a148a41fd29cf9c7f
Rehman Ali <rehmanali001@gmail.com>
2020-05-06 12:41:22 -0500

    temp

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
-Rails.application.config.middleware.use Omniauth::Builder do 
+Rails.application.config.middleware.use OmniAuth::Builder do
     provider :google_oauth2, ENV['GOOGLE_CLIENT_KEY'], ENV['GOOGLE_CLIENT_SECRET']
-end
+  end
