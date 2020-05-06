1dc779cfd4db69349fbedb1ecbcdb7939be691ac
Rehman Ali <rehmanali001@gmail.com>
2020-05-06 15:29:53 -0500

    added scope methods and fixed redirect

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,18 +5,22 @@ class BooksController < ApplicationController
         if params[:user_id] && @user = User.find_by_id(params[:user_id])
             @books = @user.books.alpha
         else  
-            @books = Book.alpha
+            @books = Book.alpha.includes(:genre, :user)
         end 
     end 
 
     def new 
-         @book = Book.new
+        if params[:user_id] && @user = User.find_by_id(params[:user_id])
+            @book = @user.books.build
+        else 
+            @book = Book.new        
+        end 
     end 
 
     def create
         @book = current_user.books.build(book_params)
         if @book.save
-            redirect_to book_path(@book)
+            redirect_to books_path
         else   
             render :new
         end 
@@ -24,16 +28,29 @@ class BooksController < ApplicationController
 
     def show 
         @book = Book.find_by_id(params[:id])
+        redirect_to books_path if !@book
     end 
 
     def edit 
+        @book = Book.find_by_id(params[:id])
+        redirect_to books_path if !@book || @book.user != current_user
+        @book.build_genre if !@book.genre
+    end 
 
+    def update 
+        @book = Book.find_by_id(id: params[:id])
+        redirect_to books_path if !@book || @book.user != current_user
+        if @book.update(book_params)
+            redirect_to book_path(@book)
+        else 
+            render :edit 
+        end 
     end 
   
 
     private 
 
     def book_params
-        params.require(:book).permit(:title, :description, :genre_id)
+        params.require(:book).permit(:title, :description, :genre_id, genre_attributes: [:name])
     end     
 end
