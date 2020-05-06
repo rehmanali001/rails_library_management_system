ea57d3d83e5381084557972cbaf6a36f89b5847e
Rehman Ali <rehmanali001@gmail.com>
2020-05-06 13:54:35 -0500

    added update methods

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -35,7 +35,11 @@ class CommentsController < ApplicationController
     end 
 
     def update
-
+        if @comment.update(comment_params)
+            redirect_to comment_path(@comment)
+        else 
+            render :edit 
+        end 
     end 
 
     private 
